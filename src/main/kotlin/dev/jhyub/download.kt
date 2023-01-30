6d7441aef2a8ab69e3e6d059ff8f96d79ff1c95a
Janghyub Seo <jhyub06@gmail.com>
2023-01-30 09:38:21 +0900

    i am not sure if this is good code

--- src/main/kotlin/dev/jhyub/download.kt
+++ src/main/kotlin/dev/jhyub/download.kt
@@ -6,8 +6,10 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.network.sockets.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -19,8 +21,19 @@ import kotlin.math.min
 // https://github.com/ktorio/ktor-documentation/blob/2.2.2/codeSnippets/snippets/client-download-file-range/src/main/kotlin/com/example/Downloader.kt
 
 suspend fun HttpClient.download(url: String, at: File, chunkSize: Int = 1024 * 1024) {
-    val head = head(url)
-    if(!head.status.isSuccess()) return
+    var head: HttpResponse? = null
+    try {
+        head = head(url)
+    } catch (cte: ConnectTimeoutException) {
+        println("Caught cte @head@download, will retry after 2 secs")
+        delay(2000L)
+        download(url, at, chunkSize)
+    } catch (hrte: HttpRequestTimeoutException) {
+        println("Caught hrte @head@download, will retry after 2 secs")
+        delay(2000L)
+        download(url, at, chunkSize)
+    }
+    if(head?.status?.isSuccess() != true) return
     val length = head.headers[HttpHeaders.ContentLength]?.toLong() as Long
     val lastByte = length - 1
 
@@ -40,7 +53,13 @@ suspend fun HttpClient.download(url: String, at: File, chunkSize: Int = 1024 * 1
                 output.write(data)
             }
         } catch (cte: ConnectTimeoutException) {
+            println("Caught cte @get@ddownload, will retry after 2 secs")
             start -= chunkSize
+            delay(2000L)
+        } catch (hrte: HttpRequestTimeoutException) {
+            println("Caught hrte @get@ddownload, will retry after 2 secs")
+            start -= chunkSize
+            delay(2000L)
         }
         if(end >= lastByte) break
         start += chunkSize
